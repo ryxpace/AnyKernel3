2427f837444bdf70a8c9d2e5f5517c2c8c47bb14
ryxpace <ryxpace@proton.me>
2025-03-10 12:28:01 +0900

    ak: support A14-A15

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=X6-sweet
+kernel.string=kaizen-test-sweet
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -12,7 +12,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=sweet
 device.name2=sweetin
-supported.versions=11 - 14
+supported.versions=14 - 15
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
